364ee7e0f1af0df1499a6731e39dca3d56227714
Connie Liou <connie.e.liou@gmail.com>
2016-07-11 15:35:32 -0700

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,15 @@
+
+var countries = 
+		[
+			{country: "argentina", capitol: "buenos aires" },
+			{country: "australia", capitol: "canberra" },
+			{country: "germany", capitol: "frankfurt" },
+			{country: "japan", capitol: "tokyo" },
+			{country: "england", capitol: "london" },
+			{country: "ireland", capitol: "dublin" },
+			{country: "canada", capitol: "ottawa" },
+			{country: "sweden", capitol: "stockholm" },
+			{country: "france", capitol: "paris" },
+			{country: "korea", capitol: "seoul" }
+		]
+
